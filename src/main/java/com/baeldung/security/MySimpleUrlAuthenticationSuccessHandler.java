317b545865ce1fbb509886eb4e2ce539366e8b89
sharifi <h_sharifi@modernisc.com>
2021-04-19 09:55:19 +0430

    add redirect url

--- src/main/java/com/baeldung/security/MySimpleUrlAuthenticationSuccessHandler.java
+++ src/main/java/com/baeldung/security/MySimpleUrlAuthenticationSuccessHandler.java
@@ -82,6 +82,7 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
     protected String determineTargetUrl(final Authentication authentication) {
         boolean isUser = false;
         boolean isAdmin = false;
+        boolean isManager = false;
         final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         for (final GrantedAuthority grantedAuthority : authorities) {
             if (grantedAuthority.getAuthority().equals("READ_PRIVILEGE")) {
@@ -90,6 +91,8 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
                 isAdmin = true;
                 isUser = false;
                 break;
+            } else if (grantedAuthority.getAuthority().equals("MANAGE_PRIVILEGE")){
+                isManager = true;
             }
         }
         if (isUser) {
@@ -104,6 +107,8 @@ public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSu
             return "/homepage.html?user="+username;
         } else if (isAdmin) {
             return "/console";
+        } else if (isManager){
+            return "/management";
         } else {
             throw new IllegalStateException();
         }
